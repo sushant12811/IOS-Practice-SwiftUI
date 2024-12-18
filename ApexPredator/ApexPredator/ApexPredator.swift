78d42201ec96e7698348d3b9311916f9d1eb54e8
sushant12811 <steve.malistella@gmail.com>
2024-12-17 19:31:33 -0500

    made a list

--- ApexPredator/ApexPredator/ApexPredator.swift
+++ ApexPredator/ApexPredator/ApexPredator.swift
@@ -6,21 +6,45 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct ApexPredator: Decodable{
+struct ApexPredator: Decodable, Identifiable{
     let id: Int
     let name: String
-    let type: String
-    let latitude: String
-    let longitude: String
+    let type: PredatorType
+    let latitude: Double
+    let longitude: Double
     let movies: [String]
-    let movieScene: [MovieScene]
+    let movieScenes: [MovieScene]
     let link: String
     
-    struct MovieScene: Decodable{
+    var image : String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    
+    
+    enum PredatorType: String, Decodable{
+        case land
+        case air
+        case sea
+    
+        var backgroundcolorType: Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
     }
     
 }
--- ApexPredator/ApexPredator/ContentView.swift
+++ ApexPredator/ApexPredator/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let predators = Predators()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        List(predators.apexPredator) { predator in
+            HStack{
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color:.white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    Text(predator.name)
+                        .fontWeight(.bold)
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal,10)
+                        .padding(.vertical,5)
+                        .background(predator.type.backgroundcolorType)
+                        .clipShape(.capsule)
+                        
+                }
+            }
+
+            
+        }.preferredColorScheme(.dark)
     }
 }
 
